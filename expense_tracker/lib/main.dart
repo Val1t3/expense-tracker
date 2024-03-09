86e9711bf23fd2d058f26fe79048692170dff1bb
Val1t3 <valentinwoehrel2003@gmail.com>
2024-03-10 00:18:41 +0100

    [MODIF] update routes to integrate new screens

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,3 +1,7 @@
+import 'package:expense_tracker/Screens/details_list_screen.dart';
+import 'package:expense_tracker/Screens/details_screen.dart';
+import 'package:expense_tracker/Screens/login_screen.dart';
+import 'package:expense_tracker/Screens/settings_screen.dart';
 import 'package:flutter/material.dart';
 
 import 'Screens/add_expense_screen.dart';
@@ -18,6 +22,10 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => const HomeScreen(),
         '/add': (context) => const AddExpenseScreen(),
+        '/details_list': (context) => const DetailsListScreen(),
+        '/details/:id': (context) => const DetailsScreen(),
+        '/settings': (context) => const SettingsScreen(),
+        '/login': (context) => const LoginScreen(),
       },
       // theme: ThemeData(
       //   primarySwatch: Colors.blue,

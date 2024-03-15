69cf9f0594174a7c05488b6d85be810d7afaa0b5
Val1t3 <valentinwoehrel2003@gmail.com>
2024-03-15 19:37:43 +0100

    [ADD] button to add an expense

--- expense_tracker/lib/Screens/add_expense_screen.dart
+++ expense_tracker/lib/Screens/add_expense_screen.dart
@@ -9,25 +9,52 @@ class AddExpenseScreen extends StatefulWidget {
 
 class _AddExpenseScreenState extends State<AddExpenseScreen> {
   List<String> list = <String>["Expense", "Income"];
+  List<String> expenses = List.empty(growable: true);
+
+  final TextEditingController _titleController = TextEditingController();
+
+  _addExpenseTest() {
+    String title = _titleController.text;
+    setState(() {
+      expenses.add(title);
+    });
+
+    // TODO: Remove this print statement
+    print(expenses);
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Container(
-      margin: const EdgeInsets.all(40.0),
-      child: const Scaffold(
-        body: Center(
+      home: Container(
+        margin: const EdgeInsets.all(40.0),
+        child: Scaffold(
+          body: Center(
             child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              decoration: InputDecoration(
-                  hintText: "Title", border: OutlineInputBorder()),
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextField(
+                  controller: _titleController,
+                  decoration: const InputDecoration(
+                    hintText: "Title",
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: _addExpenseTest,
+                  child: const Text('Add'),
+                ),
+              ],
             ),
-            // DropdownButton(items: items, onChanged: onChanged)
-          ],
-        )),
+          ),
+        ),
       ),
-    ));
+    );
   }
 }
